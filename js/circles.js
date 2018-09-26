281eec29d0410fe1cb46ae002c0368997f0a1575
Jaclyn Antreasian <jaclynantreasian@Jaclyns-MBP.fios-router.home>
2018-09-26 11:16:51 -0400

    portfolio edits ctd

---
+++ js/circles.js
@@ -0,0 +1,30 @@
+
+    $( document ).ready(function() {
+	$("#one-circle").circliful({
+                foregroundBorderWidth: 5,
+                backgroundBorderWidth: 2,
+                percent: 79,
+                foregroundColor:'#3A27A5',
+                fontColor: '#251F47'
+
+           });
+
+    $("#two-circle").circliful({
+                foregroundBorderWidth: 5,
+                backgroundBorderWidth: 2,
+                percent: 39,
+                foregroundColor:'#3A27A5',
+                fontColor: '#251F47'
+
+
+            });
+
+    $("#three-circle").circliful({
+                foregroundBorderWidth: 5,
+                backgroundBorderWidth: 2,
+                percent: 27,
+                foregroundColor:'#3A27A5',
+                fontColor: '#251F47'
+
+            });
+   });
